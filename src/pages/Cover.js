7b2847cd875d02ed9ae927b362a6547d2d70c867
Caden <35741667+cadentj@users.noreply.github.com>
2023-02-16 21:53:13 -0600

    Pushing Updated Cover from Test App
    - Deleted folder on computer and reinstalling node modules

--- src/pages/Cover.js
+++ src/pages/Cover.js
@@ -5,25 +5,22 @@ import { Plane, useAspect, useTexture, Image, OrbitControls, Text } from '@react
 import { EffectComposer, DepthOfField, Vignette } from '@react-three/postprocessing'
 
 
-import { Box } from '@mui/system'
 
 import bgUrl from '../resources/top.png'
 import starsUrl from '../resources/middle.png'
 import groundUrl from '../resources/bottom.png'
-import bearUrl from '../resources/bear.png'
-import leaves1Url from '../resources/leaves1.png'
-import leaves2Url from '../resources/leaves2.png'
 
-import Ghibli from './../fonts/ghibli/Eyad Al-Samman - Ghibli-Bold.otf'
+import Ghibli from '../fonts/ghibli/Eyad Al-Samman - Ghibli-Bold.otf'
 
 
-function Scene() {
+function Scene({ dof }) {
     const scaleN = useAspect(1600, 1000, 1.05)
     const scaleW = useAspect(2200, 1000, 1.05)
     const group = useRef()
     const subject = useRef()
+    const [focus] = useState(() => new THREE.Vector3())
 
-    const textures = [bgUrl, starsUrl, groundUrl, bearUrl, leaves1Url, leaves2Url]
+    const textures = [bgUrl, starsUrl, groundUrl]
     const layers = [
         { texture: textures[0], scale: scaleW, z: 0 },
         { texture: textures[1], scale: scaleW, z: 10 },
@@ -31,6 +28,7 @@ function Scene() {
     ]
 
     useFrame((state, delta) => {
+        dof.current.target = focus.lerp(subject.current.position, 0.05)
         group.current.position.x = THREE.MathUtils.lerp(group.current.position.x, state.mouse.x * 20, 0.2)
         group.current.rotation.x = THREE.MathUtils.lerp(group.current.rotation.x, state.mouse.y / 10, 0.2)
         group.current.rotation.y = THREE.MathUtils.lerp(group.current.rotation.y, -state.mouse.x / 2, 0.2)
@@ -40,38 +38,52 @@ function Scene() {
             {layers.map(({ texture, scale, z, scaleFactor = 1 }) => (
                 <Image url={texture} scale={scale} position-z={z} transparent={0.5} />
             ))}
-            <group ref={subject}>
-            <Text
-                scale={[15, 15, 1]}
-                position={[0, 15, 50]}
-                color="white" // default
-                font={Ghibli}
-                letterSpacing={0.3}
-            >
-                CADEN
-            </Text>
-            <Text
-                scale={[15, 15, 1]}
-                position={[0, -15, 50]}
-                color="white" // default
-                font={Ghibli}
-                letterSpacing={0.3}
-            >
-                JUANG
-            </Text>
+            <group >
+                <Text
+                    ref={subject}
+                    scale={[15, 15, 5]}
+                    position={[0, 15, 50]}
+                    color="white" // default
+                    font={Ghibli}
+                    letterSpacing={0.3}
+                >
+                    CADEN
+                </Text>
+                <Text
+                ref={subject}
+                    scale={[15, 15, 5]}
+                    position={[0, -15, 50]}
+                    color="white" // default
+                    font={Ghibli}
+                    letterSpacing={0.3}
+                >
+                    JUANG
+                </Text>
             </group>
         </group>
     )
 }
 
-export default function App() {
+const Effects = React.forwardRef((props, ref) => {
+    const { viewport: { width, height } } = useThree() // prettier-ignore
     return (
-        <Box sx={{ height: "100vh", backgroundColor: "black" }}>
+        <EffectComposer multisampling={0}>
+            <DepthOfField ref={ref} bokehScale={3} focalLength={0.1} width={(width * 5) / 2} height={(height * 5) / 2} />
+            <Vignette />
+        </EffectComposer>
+    )
+})
+
+export default function App() {
+    const dof = useRef()
+    return (
+        <div style={{ height: "100vh", backgroundColor: "black" }}>
             <Canvas orthographic camera={{ zoom: 5, position: [0, 0, 200], far: 300, near: 0 }}>
                 <Suspense>
-                    <Scene />
+                    <Scene dof={dof} />
                 </Suspense>
+                <Effects ref={dof} />
             </Canvas>
-        </Box>
+        </div>
     )
 }
